6be755331a1ce97fe419702d2346312d4d1442db
ledinhnhat1102 <nhat6870k@gmail.com>
2024-06-26 11:03:17 +0700

    hi

--- obj/Debug/net8.0/SinhVienApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SinhVienApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SinhVienApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb686b540643e65ca524756fff2fd59ea8fecb44")]
 [assembly: System.Reflection.AssemblyProductAttribute("SinhVienApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SinhVienApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
